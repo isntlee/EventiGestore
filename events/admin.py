9c835d37c387ac0e94a11aec1d9b0aceaf12bfea
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-09-23 20:01:26 +0200

    Added Event model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'start_date', 'full', 'creator')
+
+
+admin.site.register(models.Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+
+    class EventObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(active=False)
+        
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    attendees = models.ManyToManyField(User)
+    creator = models.ForeignKey(User, related_name='created_events', on_delete=models.CASCADE)
+    full = models.BooleanField(default=False)    
+    objects = models.Manager()
+    eventobjects = EventObjects()
+
+    class Meta:
+        ordering = ('start_date',)
+    
+    def __str__(self):
+        return self.name 
