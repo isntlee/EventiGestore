f7a00a98a4eda8447901cd1702377e88ec6bc81e
DESKTOP-5FRD4TM\Lee <leedavidmartina@gmail.com>
2023-09-24 15:19:37 +0200

    Added requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j=n0!0t=kc*=m07dso_770#i=0&k$hk!=0r$47jf$d=1wadr!d'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
+    'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
